cf45006f0142cf5920b1e9749a60dccbd0ce4d9c
Jackpot51 <Jackpot51@8a0a8dd9-a945-1c3f-2ffc-215740590452>
2010-06-12 21:55:45 +0000

    I have added an asteroids program that is partially functional and a screensaver. I have also broken the X11 folder into two, with the cygwin folder being for windows.

--- trunk/SollerOS/LibGUI/compile.bat
+++ trunk/SollerOS/LibGUI/compile.bat
@@ -1,8 +1,10 @@
 set PATH=%PATH%;\cygwin\bin
 set scc=\cygwin\SollerOS\cross\bin\i586-pc-solleros-gcc
+%scc% -s asteroids.c -L SollerOS -lgui -lm -o SollerOS/asteroids.elf
 %scc% -s gravity.c -L SollerOS -lgui -o SollerOS/gravity.elf
 %scc% -s raytrace.c -L SollerOS -lgui -o SollerOS/raytrace.elf
 %scc% -s screensaver.c -L SollerOS -lgui -o SollerOS/screensaver.elf
-gcc-4 -s gravity.c -L X11 -lgui -lX11 -o X11/gravity.exe
-gcc-4 -s raytrace.c -L X11 -lgui -lX11 -o X11/raytrace.exe
-gcc-4 -s screensaver.c -L X11 -lgui -lX11 -o X11/screensaver.exe
+gcc-4 -s asteroids.c -L cygwin -lgui -lX11 -lm -o cygwin/asteroids.exe
+gcc-4 -s gravity.c -L cygwin -lgui -lX11 -o cygwin/gravity.exe
+gcc-4 -s raytrace.c -L cygwin -lgui -lX11 -o cygwin/raytrace.exe
+gcc-4 -s screensaver.c -L cygwin -lgui -lX11 -o cygwin/screensaver.exe
