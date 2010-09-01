089745ec4ba6f12b31c5d1905eecc00b58177e8a
Jackpot51 <Jackpot51@8a0a8dd9-a945-1c3f-2ffc-215740590452>
2010-09-01 02:27:16 +0000

    I have fixed stuff up for windows. GDI is not spawning the window though.

--- SollerOS/LibGUI/compile-linux.sh
+++ SollerOS/LibGUI/compile-linux.sh
@@ -1,3 +1,17 @@
+<<<<<<< .mine
+#scc="/SollerOS/cross/bin/i586-pc-solleros-gcc -LSollerOS -lgui"
+#$scc -s 3d.c -lm -o SollerOS/3d.elf
+#$scc -s asteroids.c -lm -o SollerOS/asteroids.elf
+#$scc -s gravity.c -o SollerOS/gravity.elf
+#$scc -s raytrace.c -o SollerOS/raytrace.elf
+#$scc -s screensaver.c -o SollerOS/screensaver.elf
+flags="-LX11 -lgui -lX11 -s" 
+gcc 3d.c -o X11/3d.elf $flags -lm
+gcc asteroids.c -o X11/asteroids.elf $flags -lm
+gcc gravity.c -o X11/gravity.elf $flags
+gcc raytrace.c -o X11/raytrace.elf $flags
+gcc screensaver.c -o X11/screensaver.elf $flags
+=======
 scc="/SollerOS/cross/bin/i586-pc-solleros-gcc -LSollerOS -lgui"
 $scc -s 3d.c -lm -o SollerOS/3d.elf
 $scc -s asteroids.c -lm -o SollerOS/asteroids.elf
@@ -11,3 +25,4 @@ $hcc -s asteroids.c -lm -o X11/asteroids.elf
 $hcc -s gravity.c -o X11/gravity.elf
 $hcc -s raytrace.c -o X11/raytrace.elf
 $hcc -s screensaver.c -o X11/screensaver.elf
+>>>>>>> .r23
