7ea6515c6dbd92595b8abbec2f6963eac4fd09da
Jackpot51 <Jackpot51@8a0a8dd9-a945-1c3f-2ffc-215740590452>
2010-09-01 04:34:54 +0000

    I moved the LibGUI stuff around. I am in the process of cleaning up the build.

--- trunk/SollerOS/LibGUI/OSX/compile.sh
+++
@@ -1,5 +0,0 @@
-gcc -c libgui.c -o libgui.o
-ar rcs libgui.a libgui.o
-rm libgui.o
-cd ..
-./compile-osx.sh
--- trunk/SollerOS/LibGUI/SollerOS/compile.sh
+++
@@ -1,4 +0,0 @@
-scc="/SollerOS/cross/bin/i586-pc-solleros-gcc"
-$scc -c libgui.c -o libgui.o
-ar rcs libgui.a libgui.o
-rm libgui.o
