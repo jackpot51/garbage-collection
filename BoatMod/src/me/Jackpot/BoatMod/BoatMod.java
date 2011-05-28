f0687879e22c405734098c2558a00a8fcf509daa
jackpot51 <jackpot51@8a0a8dd9-a945-1c3f-2ffc-215740590452>
2011-05-28 19:11:14 +0000

    Did a cleanup to increase speed and refactored to make loading certain values from a file easier.

--- BoatMod/src/me/Jackpot/BoatMod/BoatMod.java
+++ BoatMod/src/me/Jackpot/BoatMod/BoatMod.java
@@ -1,20 +1,109 @@
 package me.Jackpot.BoatMod;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.logging.Logger;
 
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class BoatMod extends JavaPlugin {
 	private final BoatModPlayerListener playerListener = new BoatModPlayerListener(this);
+	Hashtable<Player, Boat> _boats;
 	Logger log = Logger.getLogger("Minecraft");
 	public void onEnable(){
 		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.Normal, this);
+		_boats = new Hashtable<Player, Boat>();
+		pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.High, this);
+		pm.registerEvent(Event.Type.PLAYER_KICK, playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.PLAYER_QUIT, playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.PLAYER_RESPAWN, playerListener, Event.Priority.Normal, this);
 		log.info("BoatMod has been enabled.");
 	}
 	public void onDisable(){
+		for(Enumeration<Player> players = _boats.keys(); players.hasMoreElements();){
+			RemoveBoat(players.nextElement());
+		}
 		log.info("BoatMod has been disabled.");
+	}
+	
+	public void Message(Player player, String message){
+		player.sendMessage("§2[BoatMod] §c" + message);
+	}
+	
+	private Boat getBoat(Player captain){
+		if(_boats.containsKey(captain)){
+			return _boats.get(captain);
+		}else{
+			return null;
+		}
+	}
+	
+	private Vector GetCompassDirection(Vector dir){
+		Vector vec = new Vector(0,0,0);
+		if(dir.getX() > 0.75){
+			vec.setX(1);
+		}
+		if(dir.getX() < -0.75){
+			vec.setX(-1);
+		}
+
+		if(dir.getY() > 0.75){
+			vec.setY(1);
+		}
+		if(dir.getY() < -0.75){
+			vec.setY(-1);
+		}
+
+		if(dir.getZ() > 0.75){
+			vec.setZ(1);
+		}
+		if(dir.getZ() < -0.75){
+			vec.setZ(-1);
+		}
+		return vec;
+	}
+	
+	public int MaxBoatSize(Player player){
+		if(player.isOp()){
+			return 16384;
+		}else{
+			return 2048;
+		}
+	}
+	
+	public void AddBoat(Player player, Block block){
+		Boat boat = getBoat(player);
+		if(boat != null){
+			_boats.remove(player);
+		}
+		boat = new Boat(block, player, this);
+		_boats.put(player, boat);
+		log.info("[BoatMod] " + player.getDisplayName() + " created a boat of size " + boat._vectors.size() + " blocks.");
+	}
+	
+	public void RemoveBoat(Player player){
+		Boat boat = getBoat(player);
+		if(boat != null){
+			_boats.remove(player);
+			Message(player, "Your boat has been deboated.");
+			Message(player, "You have been demoted.");
+			log.info("[BoatMod] " + player.getDisplayName() + "'s boat was removed.");
+		}
+	}
+	
+	public void MoveBoat(Player player){
+		Boat boat = getBoat(player);
+		Vector v = player.getLocation().getDirection();
+		boat.Move(GetCompassDirection(v));
+	}
+	
+	public void ChangeSpeed(Player player){
+		Boat boat = getBoat(player);
+		boat.changeSpeed();
 	}
 }
--- BoatMod/src/me/Jackpot/BoatMod/BoatModPlayerListener.java
+++ BoatMod/src/me/Jackpot/BoatMod/BoatModPlayerListener.java
@@ -1,81 +1,48 @@
 package me.Jackpot.BoatMod;
 
-import java.util.Hashtable;
-
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerListener;
-import org.bukkit.util.Vector;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class BoatModPlayerListener extends PlayerListener{
 	public static BoatMod plugin;
-	Hashtable<Player, Boat> _boats;
 	public BoatModPlayerListener(BoatMod instance){
 		plugin = instance;
-		_boats = new Hashtable<Player, Boat>();
-	}
-
-	private Vector GetCompassDirection(Vector dir){
-		Vector vec = new Vector(0,0,0);
-		if(dir.getX() > 0.75){
-			vec.setX(1);
-		}
-		if(dir.getX() < -0.75){
-			vec.setX(-1);
-		}
-
-		if(dir.getY() > 0.75){
-			vec.setY(1);
-		}
-		if(dir.getY() < -0.75){
-			vec.setY(-1);
-		}
-
-		if(dir.getZ() > 0.75){
-			vec.setZ(1);
-		}
-		if(dir.getZ() < -0.75){
-			vec.setZ(-1);
-		}
-		return vec;
-	}
-
-	int movespeed = 2; //this will ensure that it moves as fast as a minecaft does maxxed out.
-	
-	private Boat getBoat(Player captain){
-		if(_boats.containsKey(captain)){
-			return _boats.get(captain);
-		}else{
-			return null;
-		}
 	}
 	
 	public void onPlayerInteract(PlayerInteractEvent event){
 		Player player = event.getPlayer();
-		Boat boat = getBoat(player);
-		if(event.getMaterial() == Material.BOAT && event.getAction() == Action.LEFT_CLICK_BLOCK){
-			if(boat != null){
-				_boats.remove(player);
-			}
-			_boats.put(player, new Boat(event.getClickedBlock(), event.getClickedBlock().getWorld(), player, plugin));
-		}
-		if(boat != null){
-			if(event.getMaterial() == Material.BOAT && event.getAction() == Action.LEFT_CLICK_AIR){
-				_boats.remove(event.getPlayer());
-				player.sendMessage("[BoatMod] Your boat has been deboated.");
-				player.sendMessage("[BoatMod] You have been demoted.");
-			}
-			if(event.getMaterial() == Material.MAP){
-				Vector v = player.getLocation().getDirection();
-				if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK){
-					boat.Move(GetCompassDirection(v));
-				}
-				else if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK){
-					boat.changeSpeed();
-				}
+		if(event.getMaterial() == Material.BOAT){
+			if(event.getAction() == Action.LEFT_CLICK_BLOCK){
+				plugin.AddBoat(player, event.getClickedBlock());
+			}else if(event.getAction() == Action.LEFT_CLICK_AIR){
+				plugin.RemoveBoat(player);
 			}
 		}
+		else if(event.getMaterial() == Material.MAP){
+			if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK){
+				plugin.MoveBoat(player);
+			}
+			else if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK){
+				plugin.ChangeSpeed(player);
+			}
+		}
+	}
+	
+	public void onPlayerKick(PlayerKickEvent event){
+		plugin.RemoveBoat(event.getPlayer());
+	}
+	
+	public void onPlayerQuit(PlayerQuitEvent event){
+		plugin.RemoveBoat(event.getPlayer());
+	}
+	
+	public void onPlayerRespawn(PlayerRespawnEvent event){
+		plugin.RemoveBoat(event.getPlayer());
 	}
 }
