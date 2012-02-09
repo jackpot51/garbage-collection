cd582178061703be66c3a08e9482ddfee249d246
jackpot51@gmail.com <jackpot51@gmail.com@8a0a8dd9-a945-1c3f-2ffc-215740590452>
2012-02-09 23:44:21 +0000

    Added autopilot class

---
+++ BoatMod/src/me/Jackpot/BoatMod/AutoPilot.java
@@ -0,0 +1,32 @@
+package me.Jackpot.BoatMod;
+
+import org.bukkit.util.Vector;
+
+
+public class AutoPilot implements Runnable{
+	public static BoatMod plugin;
+	int _autopilot;
+	Boat _boat;
+	LocalVector _lv;
+	
+	public AutoPilot(Boat boat, BoatMod instance){
+		plugin = instance;
+		_boat = boat;
+		_lv = new LocalVector(_boat._dir.clone());
+	}
+	public void set(boolean autopilot){
+		if(autopilot ^ (_autopilot != 0)){
+			if(autopilot){
+				_autopilot = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, 10);
+			}else{
+				plugin.getServer().getScheduler().cancelTask(_autopilot);
+				_autopilot = 0;
+			}
+		}
+	}
+	
+	@Override
+	public void run() {
+		_boat.Move((_lv.toReal(new Vector(0, 0, 0), _boat.lasttheta)));
+	}
+}
