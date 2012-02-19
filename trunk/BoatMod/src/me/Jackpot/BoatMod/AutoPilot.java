32dc8ccd06ea9ad7dc364a0e53f60192cbe986b7
jackpot51@gmail.com <jackpot51@gmail.com@8a0a8dd9-a945-1c3f-2ffc-215740590452>
2012-02-19 05:59:12 +0000

    Some cleanup, air processing temporarily removed

--- trunk/BoatMod/src/me/Jackpot/BoatMod/AutoPilot.java
+++ trunk/BoatMod/src/me/Jackpot/BoatMod/AutoPilot.java
@@ -14,14 +14,18 @@ public class AutoPilot implements Runnable{
 		_boat = boat;
 		_lv = new LocalVector(_boat._dir.clone());
 	}
-	public void set(boolean autopilot, int ticks){
-		if(autopilot ^ (_autopilot != 0)){
-			if(autopilot){
-				_autopilot = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, ticks);
-			}else{
-				plugin.getServer().getScheduler().cancelTask(_autopilot);
-				_autopilot = 0;
-			}
+	
+	public void start(int ticks){
+		if(_autopilot != 0){
+			stop();
+		}
+		_autopilot = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, ticks);
+	}
+	
+	public void stop(){
+		if(_autopilot != 0){
+			plugin.getServer().getScheduler().cancelTask(_autopilot);
+			_autopilot = 0;
 		}
 	}
 	
